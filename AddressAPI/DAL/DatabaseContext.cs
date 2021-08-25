f0284b91e8584016cfccbe058d56dd3404ad6305
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-08-25 12:07:09 +0200

    Add project files.

---
+++ AddressAPI/DAL/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AddressAPI.DAL
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> configuration):base(configuration)
+        {
+
+        }
+    }
+}
---
+++ AddressAPI/Model/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace AddressAPI.Model
+{
+    [Table("Address")]
+    public class Address
+    {
+        [Key]
+        [Required]
+        [SwaggerSchema(ReadOnly = true)]
+        public int AddressId { get; set; }
+
+        [StringLength(50)]
+        public string Street { get; set; }
+
+        public int HouseNumber { get; set; }
+
+        [StringLength(3)]
+        public string Annex { get; set; }
+
+        [StringLength(7)]
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+    }
+}
